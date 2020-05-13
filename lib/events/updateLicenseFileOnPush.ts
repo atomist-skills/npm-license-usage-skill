1a22d4aff9be26c43cf7d70173da50ffd67a2772
Christian Dupuis <cd@atomist.com>
2020-05-13 18:49:21 +0200

    Add audit logging

--- lib/events/updateLicenseFileOnPush.ts
+++ lib/events/updateLicenseFileOnPush.ts
@@ -45,6 +45,8 @@ export const handler: EventHandler<UpdateLicenseFileOnPushSubscription, Configur
         };
     }
 
+    await ctx.audit.log(`Starting NPM license usage update on ${repo.owner}/${repo.name}`);
+
     const credential = await ctx.credential.resolve(gitHubAppToken({
         owner: repo.owner,
         repo: repo.name,
@@ -57,6 +59,8 @@ export const handler: EventHandler<UpdateLicenseFileOnPushSubscription, Configur
         credential,
     }));
 
+    await ctx.audit.log(`Cloned repository ${repo.owner}/${repo.name} at sha ${push.after.sha.slice(0, 7)}`);
+
     if (!(await fs.pathExists(project.path("package.json")))) {
         return {
             code: 0,
@@ -68,6 +72,7 @@ export const handler: EventHandler<UpdateLicenseFileOnPushSubscription, Configur
     await addThirdPartyLicenseFile(project, ctx);
 
     if (!(await git.status(project)).isClean) {
+        await ctx.audit.log(`Updated NPM license usage`);
         const commitMsg = `Update NPM license usage for ${push.after.sha.slice(0, 7)}\n\n[atomist:generated]\n[atomist-skill:${ctx.skill.namespace}/${ctx.skill.name}]`;
         if (cfg.push.includes("commit")) {
             await git.commit(project, commitMsg);
@@ -94,6 +99,7 @@ export const handler: EventHandler<UpdateLicenseFileOnPushSubscription, Configur
                     pull_number: pr.number, // eslint-disable-line @typescript-eslint/camelcase
                     reviewers: [push.after.author.login],
                 });
+                await ctx.audit.log(`Raised pull request [#${pr.number}](${pr.html_url})`);
                 return {
                     code: 0,
                     reason: `Pushed NPM license usage file to [${repo.owner}/${repo.name}](${repo.url}) and raised PR [#${pr.number}](${pr.html_url})`,
@@ -108,9 +114,10 @@ export const handler: EventHandler<UpdateLicenseFileOnPushSubscription, Configur
         };
     }
 
+    await ctx.audit.log(`No change to NPM license usage`);
     return {
         code: 0,
         visibility: "hidden",
-        reason: `No change to NPM license usage file in [${repo.owner}/${repo.name}](${repo.url})`,
+        reason: `No change to NPM license usage in [${repo.owner}/${repo.name}](${repo.url})`,
     };
 };
